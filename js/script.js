91d69120574e11352d6530bfd1b2124186992f34
DorzelRamos <dorzel.ramos@gmail.com>
2022-09-07 12:44:55 -0500

    added gsap and scroll trigger

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const container1 = document.getElementById("container1");
+const container2 = document.getElementById("container2");
+//const container = document.querySelectorAll("meta");
+//console.log(container);
+//I like pizza
+
+let tl1 = gsap.timeline();
+    tl1.from(container1, {
+        autoAlpha: 0,
+         duration: 3
+        }
+        );
+let tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: container2, 
+        start: "top 75%",
+        end: "bottom bottom",
+        markers: {
+            startColor: "red",
+            endColor: "green",
+            fontSize: "18px", 
+            fontWeight: "bold",
+            indent: 28
+        }
+    }
+});
+    tl2.fromTo(
+        container2
+    )
+    {
+        //from state
+        autoAlpha: 0,
